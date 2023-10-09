b006ce4fa94deefc06abbfb68a6cb489c486a53f
MijaVujnovic <mija.vujnovic2@gmail.com>
2023-10-09 20:06:34 +0200

    vjezba1,izracunat broj redova

--- Vjezbe1MIP/Vjezbe1MIP/vj.c
+++ Vjezbe1MIP/Vjezbe1MIP/vj.c
@@ -1,8 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+
+typedef struct {
+	char ime[10];
+	char prezime[10];
+	double bodovi;
+}Student;
+
 
 int main()
 {
-	printf("Hello world");
+	int brojRedova=0;
+	char charBuffer[1024]="";
+	//printf("%d", brojRedova);
+	FILE* filePointer = NULL;
+	filePointer = fopen("text.txt", "r");
+	if (filePointer == NULL) {
+		printf("Datoteka nije pronadena");
+		return 1;
+	}
+
+	while (!feof(filePointer)) {
+		if (fgets(charBuffer, 1024, filePointer)) {
+			brojRedova++;
+		}
+
+	}
+	printf("%d", brojRedova);
+	fclose(filePointer);
 
 	return 0;
 }
