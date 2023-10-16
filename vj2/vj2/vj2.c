5c5c4653192d526928ab109d46b99c935a0dbea2
MijaVujnovic <mija.vujnovic2@gmail.com>
2023-10-16 19:58:37 +0200

    pocetak 2 zadatka

---
+++ vj2/vj2/vj2.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define MAX_LENGTH 50
+
+/*2. Definirati strukturu osoba (ime, prezime, godina rođenja) i napisati program koji:
+A. dinamički dodaje novi element na početak liste,
+B. ispisuje listu,
+C. dinamički dodaje novi element na kraj liste,
+D. pronalazi element u listi (po prezimenu),
+E. briše određeni element iz liste,
+U zadatku se ne smiju koristiti globalne varijable.*/
+
+typedef struct {
+	char name[MAX_LENGTH];
+	char surname[MAX_LENGTH];
+	int birthYear;
+
+}_Person;
+
+int main()
+{
+
+
+
+}
